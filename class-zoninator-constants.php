30e820bb9834438b38dd995e50d68caa05455d0b
Panos Kountanis <panosktn@gmail.com>
2016-01-13 19:53:50 +0000

    constantize zoninator text domain

--- class-zoninator-constants.php
+++ class-zoninator-constants.php
@@ -6,4 +6,5 @@ class Zoninator_Constants
     const ZONE_TAXONOMY = 'zoninator_zones';
     const NONCE_PREFIX = 'zone-nonce';
     const ZONE_AJAX_NONCE_ACTION = 'ajax-action';
+    const TEXT_DOMAIN = 'zoninator';
 }
