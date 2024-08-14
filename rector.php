2ca2c1dadeffa352d6f428d366f29cb32732d284
Gary Jones <github@garyjones.io>
2024-08-14 11:23:51 +0100

    CS: Rector with CodingStyle
    EncapsedStringsToSprintfRector

--- rector.php
+++ rector.php
@@ -2,9 +2,13 @@
 
 declare(strict_types=1);
 
+use Rector\CodingStyle\Rector\ClassMethod\MakeInheritedMethodVisibilitySameAsParentRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
+use Rector\CodingStyle\Rector\String_\SymplifyQuoteEscapeRector;
 use Rector\Config\RectorConfig;
 use Rector\Php54\Rector\Array_\LongArrayToShortArrayRector;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\Visibility\Rector\ClassMethod\ExplicitPublicClassMethodRector;
 
 return RectorConfig::configure()
 	->withPaths(
@@ -24,9 +28,12 @@ return RectorConfig::configure()
 			DisallowedEmptyRuleFixerRector::class => array(
 				__DIR__ . '/lib/zoninator_rest/type/class-zoninator-rest-type-registry.php',
 			),
+			MakeInheritedMethodVisibilitySameAsParentRector::class,
+			NewlineAfterStatementRector::class,
+			SymplifyQuoteEscapeRector::class,
+			ExplicitPublicClassMethodRector::class,
 		)
 	)
 	->withPhpSets( php74: true )
-	->withPreparedSets( deadCode: true )
-	->withCodeQualityLevel( 70 )
+	->withPreparedSets( deadCode: true, codeQuality: true, instanceOf: true, codingStyle: true )
 	->withTypeCoverageLevel( 1 );
